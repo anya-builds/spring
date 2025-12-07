edc3823f2b349f5fd848e36fa2700e44f285b8dc
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-07 18:37:47 +0530

    spring

---
+++ src/main/java/com/demo/first/AppStartupRunner.java
@@ -0,0 +1,9 @@
+package com.demo.first;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupRunner implements CommandLineRunner {
+
+}
