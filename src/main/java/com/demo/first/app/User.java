4d0c6ab007aa91831e76dcf10d12ad6f1a28b141
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-08 22:24:50 +0530

    APIs in Spring

--- src/main/java/com/demo/first/app/User.java
+++ src/main/java/com/demo/first/app/User.java
@@ -1,26 +1,26 @@
-//package com.demo.first.app;
-//
-////POJ--> plain old java Objects
-//public class User {
-//    private int id;
-//    private String name;
-//    private String email;
-//
-//    public User(int id, String email, String name) {
-//        this.id = id;
-//        this.email = email;
-//        this.name = name;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//}
+package com.demo.first.app;
+
+//POJ--> plain old java Objects
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
