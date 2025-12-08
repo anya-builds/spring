e82bad825e3b5b37fcffda39fedd0dc88e0acc84
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-08 22:24:38 +0530

    APIs in Spring

--- src/main/java/com/demo/first/app/UserController.java
+++ src/main/java/com/demo/first/app/UserController.java
@@ -1,22 +1,22 @@
-//package com.demo.first.app;
-//
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/user")
-//public class UserController {
-//    private Map<Integer, User> userDb = new HashMap<>();
-//
-//    @PostMapping
-//    public String  createUser(@RequestBody User user){
-//        System.out.println(user.getEmail());
-//        userDb.put(user.getId(), user);
-//        return "User Created!!";
-//    }
-//}
+package com.demo.first.app;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private Map<Integer, User> userDb = new HashMap<>();
+
+    @PostMapping
+    public String  createUser(@RequestBody User user){
+        System.out.println(user.getEmail());
+        userDb.put(user.getId(), user);
+        return "User Created!!";
+    }
+}
