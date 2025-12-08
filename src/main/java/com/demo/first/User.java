0d3d1d9e2311fbe4066ef98e5aef6442dcec9c3c
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-08 17:47:55 +0530

    APIs in Spring

---
+++ src/main/java/com/demo/first/User.java
@@ -0,0 +1,13 @@
+package com.demo.first;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+}
