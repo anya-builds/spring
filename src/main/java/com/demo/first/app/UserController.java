904dff79d6e7f5257730f1cd09b04859a15f99bd
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-09 23:15:25 +0530

    APIs in Spring

--- src/main/java/com/demo/first/app/UserController.java
+++ src/main/java/com/demo/first/app/UserController.java
@@ -1,47 +1,47 @@
-//package com.demo.first.app;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.HttpStatusCode;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/user")
-//public class UserController {
-//    private Map<Integer, User> userDb = new HashMap<>();
-//
-//    @PostMapping
-//    public ResponseEntity<User>  createUser(@RequestBody User user){
-//        System.out.println(user.getEmail());
-//        userDb.putIfAbsent(user.getId(), user);
-////        return ResponseEntity.status(HttpStatus.CREATED).body(user);
-//        return new ResponseEntity<>(user, HttpStatus.CREATED);
-//    }
-//
-//
-//    @PutMapping
-//    public ResponseEntity<User> updateUser(@RequestBody User user){
-//        if(!userDb.containsKey(user.getId()))
-//            return ResponseEntity.notFound().build();
-//            userDb.put(user.getId(), user);
-//        return ResponseEntity.status(HttpStatus.OK).body(user);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String deleteUser(@PathVariable int id){
-//        if (userDb.containsKey(id))
-//            return "User not found!";
-//        userDb.remove(id);
-//        return "User Deleted!";
-//    }
-//
-//    @GetMapping
-//    public List<User> getUsers(){
-//        return new ArrayList<>(userDb.values());
-//    }
-//}
+package com.demo.first.app;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private Map<Integer, User> userDb = new HashMap<>();
+
+    @PostMapping
+    public ResponseEntity<User>  createUser(@RequestBody User user){
+        System.out.println(user.getEmail());
+        userDb.putIfAbsent(user.getId(), user);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        if(!userDb.containsKey(user.getId()))
+            return ResponseEntity.notFound().build();
+            userDb.put(user.getId(), user);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable int id){
+        if (userDb.containsKey(id))
+            return "User not found!";
+        userDb.remove(id);
+        return "User Deleted!";
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return new ArrayList<>(userDb.values());
+    }
+}
