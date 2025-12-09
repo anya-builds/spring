8e6f286e3be2cbf1a9905d884c253e491db53dc7
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-09 23:16:10 +0530

    APIs in Spring

--- src/main/java/com/demo/first/HelloController.java
+++ src/main/java/com/demo/first/HelloController.java
@@ -1,21 +1,21 @@
-package com.demo.first;
-
-import com.demo.first.app.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class HelloController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello World!!";
-    }
-//    @GetMapping("/user")
-    @RequestMapping(value="/user", method= RequestMethod.GET)
-    public User getUser(){
-        return new User(1,"Ankita","ankita@example.com");
-    }
-}
+//package com.demo.first;
+//
+//import com.demo.first.app.User;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class HelloController {
+//    @GetMapping("/hello")
+//    public String sayHello(){
+//        return "Hello World!!";
+//    }
+////    @GetMapping("/user")
+//    @RequestMapping(value="/user", method= RequestMethod.GET)
+//    public User getUser(){
+//        return new User(1,"Ankita","ankita@example.com");
+//    }
+//}
