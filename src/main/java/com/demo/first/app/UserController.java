a034137e2067931e0f4fe19258750f4b6e00028f
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-12 12:57:06 +0530

    Controllers in Spring

--- src/main/java/com/demo/first/app/UserController.java
+++ src/main/java/com/demo/first/app/UserController.java
@@ -1,14 +1,13 @@
 package com.demo.first.app;
 
+import jakarta.websocket.OnClose;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/user")
@@ -87,5 +86,17 @@ public class UserController {
         return "User Agent: "+userAgent
                 +" : "+id
                 +" : "+name;
+    }
+    //exception handling method
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(
+            IllegalArgumentException exception
+    ){
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status",HttpStatus.BAD_REQUEST.value());
+        errorResponse.put("error", "Bad Request");
+        errorResponse.put("message", exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
