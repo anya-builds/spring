10df021ff64f30ee03a2836643b303f5cff79232
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-30 23:30:18 +0530

    spring

--- src/main/java/com/demo/first/app/model/Post.java
+++ src/main/java/com/demo/first/app/model/Post.java
@@ -9,6 +9,8 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String title;
+    private String content;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -22,4 +24,53 @@ public class Post {
 
     )
     private List<Tag> tags;
+
+    public Post(){
+
+    }
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 }
--- src/main/java/com/demo/first/app/model/Tag.java
+++ src/main/java/com/demo/first/app/model/Tag.java
@@ -9,7 +9,39 @@ public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String name;
 
     @ManyToMany(mappedBy = "tags")
     private List<Post> posts;
+
+    public Tag() {
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
