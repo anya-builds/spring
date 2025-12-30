b25887a15570f319fec4e4590afc5ea2130110d1
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-30 23:20:28 +0530

    spring

--- src/main/java/com/demo/first/app/model/User.java
+++ src/main/java/com/demo/first/app/model/User.java
@@ -1,26 +1,10 @@
 package com.demo.first.app.model;
 
-import jakarta.persistence.*;
-
-import java.util.List;
-
 //POJO --> Plain Old Java Objects
-@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
-
-    public User() {
-    }
-
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
-    private Profile profile;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Post> posts;
 
     public User(int id, String name, String email) {
         this.id = id;
@@ -38,33 +22,5 @@ public class User {
 
     public String getName() {
         return name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<Post> posts) {
-        this.posts = posts;
-    }
-
-    public Profile getProfile() {
-        return profile;
-    }
-
-    public void setProfile(Profile profile) {
-        this.profile = profile;
     }
 }
