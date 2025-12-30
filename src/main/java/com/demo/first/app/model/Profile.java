c61b9705160e6aca8cc5d3ac522275fac74b4e45
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-30 14:31:20 +0530

    spring

--- src/main/java/com/demo/first/app/model/Profile.java
+++ src/main/java/com/demo/first/app/model/Profile.java
@@ -1,13 +1,14 @@
 package com.demo.first.app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
